5cec7cfb05b67b4b775abc1fe2a4b7e829cc335c
Hugo <hugopalominogalindo974@gmail.com>
2025-01-07 22:21:10 -0500

    decorador Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
+import { TODO_DATA } from '../assets/todo';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,7 @@ import { TodoComponent } from './pages/todo/todo.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'curseAngular';
+  todoData1 = TODO_DATA[0];
+  todoData2 = TODO_DATA[1];
+
 }
--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { NTodo } from './../../models/todo.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+
+/*decorador de component*/
 @Component({
   selector: 'app-todo',
   standalone: true,
@@ -8,5 +11,27 @@ import { Component } from '@angular/core';
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
+
+  /***@Input => */
+  /*         => es un decorador de angular que sirve para
+  /*            pasar datos de un componente padre a un
+  /*            componente hijo. (Permite que un componente
+  /*            hijo reciba datos de un componente que lo
+  /*            contiene)
+
+  /*creamos una */
+  @Input({ required: true }) todoData?: NTodo.TodoData;
+
+    /***@Output => */
+  /*         => es un decorador de angular que sirve para
+  /*            pasar datos de un componente padre a un
+  /*            componente hijo. (Permite que un componente
+  /*            hijo reciba datos de un componente que lo
+  /*            contiene)
+
+  /*creamos una */
+  @Output() onClickIcon = new EventEmitter<boolean>();
+
+
 
 }
